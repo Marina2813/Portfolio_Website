a28aca66f9f043e714d5050b7ae29b5bbbe7deaa
marina <marinaroseshaju123@gmail.com>
2024-07-01 15:52:17 +0530

    initial

--- app/page.js
+++ app/page.js
@@ -1,11 +1,25 @@
 "use client";
 import {useRouter} from "next/navigation";
+import Navbar from "./common/Navbar";
 
 export default function Home() {
   const router = useRouter();
   return (
-    <button type="button" onClick={() => router.push('/work')}>
-      work
-    </button>
+    
+    <div className="bg-primarybg h-screen w-screen flex justify-center">
+      <Navbar/>
+      <div className="container mx-auto px-4 flex flex-col md:flex-row items-center">
+        <div className="text-left md:w-1/2 pl-16">
+          <h1 className="text-textcolor text-4xl md:text-6xl font-normal">
+            Hi, my name is <br />
+            <span className="block font-extrabold mt-4">Marina Rose</span>
+          </h1>
+          <p className="text-black text-sm md:text-base mt-2">
+            Crafting code, cooking recipes, and smashing <br/>
+            shuttlecocks—B.Tech CSE student at MITS! 🏸👩‍💻🍳
+          </p>
+        </div>
+    </div>
+    </div>
   );
 }
